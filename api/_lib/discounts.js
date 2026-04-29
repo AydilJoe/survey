a33e8db189f5dd8e2ad7bde95329b0be4ca33d57
AydilJoe <aydiljohari@gmail.com>
2026-04-29 18:25:06 +0800

    Update discounts.js

--- api/_lib/discounts.js
+++ api/_lib/discounts.js
@@ -25,7 +25,7 @@ const CODES = {
     type: "percent",
     off: 100,
     description: "Family & friends — free Pro",
-    expires: "2026-04-30",
+    expires: "2026-03-30",
   },
     MRAWHB: {
     type: "fixed",
